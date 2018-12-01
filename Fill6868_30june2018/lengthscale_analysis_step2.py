94f165806d4ffaa847b9aa1edeb61db5b65bd12b
nsahoo <niladribihari.sahoo@cern.ch>
2018-12-01 14:28:48 +0100

    updated

--- Fill6868_30june2018/lengthscale_analysis_step2.py
+++ Fill6868_30june2018/lengthscale_analysis_step2.py
@@ -12,7 +12,13 @@ def generateOffsetPositions(whichScan):
 
 def makeCalibPlot(whichScan,rootOutFile,pdfOutFile):
 
-    whichVtx = "vtx_y" #hardcoded, change accordingly for a X scan to "vtx_x"
+    ###whichVtx = "vtx_y" #hardcoded, change accordingly for a X scan to "vtx_x"
+
+    if 'X' in whichScan:
+        whichVtx = "vtx_x"
+
+    if 'Y' in whichScan:
+        whichVtx = "vtx_y"
 
     nomOff = generateOffsetPositions(whichScan)
     
@@ -29,18 +35,19 @@ def makeCalibPlot(whichScan,rootOutFile,pdfOutFile):
     vtxVsOff_B2=r.TGraphErrors()
     vtxVsOff_B2.SetTitle("LS scan " + whichScan + " \"Fro\": Mean " + whichVtx + " position in microns vs nominal offset in microns")
 
-    
-    b1y = [-593.454123687, -471.98784292, -349.796310151, -226.333529033, -103.255592813 ] # bump position of beam 1 in Y
-    b1y1= [0.252894444764, 0.255665623143, 0.251476809901, 0.260806032363, 0.298014380403 ] # bump error of beam 1  in Y
+    if whichVtx == "vtx_y":
+        b1y = [-593.454123687, -471.98784292, -349.796310151, -226.333529033, -103.255592813 ] # bump position of beam 1 in Y
+        b1y1= [0.252894444764, 0.255665623143, 0.251476809901, 0.260806032363, 0.298014380403 ] # bump error of beam 1  in Y
         
-    b2y = [-594.464129834, -472.493897786, -349.650709727, -226.419710847, -103.582486839] # bump position of beam 2 in Y
-    b2y1 = [0.140853, 0.256386, 0.192506, 0.330351, 0.163008] # bump error of beam 2  in Y
+        b2y = [-594.464129834, -472.493897786, -349.650709727, -226.419710847, -103.582486839] # bump position of beam 2 in Y
+        b2y1 = [0.140853, 0.256386, 0.192506, 0.330351, 0.163008] # bump error of beam 2  in Y
 
-    #b1y = [1051.72577846, 929.535432632, 807.529, 685.191900906, 562.731948333]# bump position of beam 1 in X
-    #b1y1 = [0.142445, 0.250039,  0.471316,  0.324029, 0.26413] #bump error of beam 1  in X
+    if whichVtx == "vtx_x":
+        b1y = [1051.72577846, 929.535432632, 807.529, 685.191900906, 562.731948333]# bump position of beam 1 in X
+        b1y1 = [0.142445, 0.250039,  0.471316,  0.324029, 0.26413] #bump error of beam 1  in X
     
-    #b2y=[1051.84044706 ,929.848598418, 807.119033348, 685.405552484, 562.247677427] # bump position of beam 2 in X
-    #b2y1=[0.288837663739 ,0.405783741016, 0.403196975094 , 0.286191565656, 0.291562812398] # bump error of beam 2  in X
+        b2y=[1051.84044706 ,929.848598418, 807.119033348, 685.405552484, 562.247677427] # bump position of beam 2 in X
+        b2y1=[0.288837663739 ,0.405783741016, 0.403196975094 , 0.286191565656, 0.291562812398] # bump error of beam 2  in X
 
     for i in range(0,5):
 
@@ -85,11 +92,15 @@ def makeCalibPlot(whichScan,rootOutFile,pdfOutFile):
     stats = vtxVsOff_B1.GetListOfFunctions().FindObject('stats')
     #stats.SetName("mystats")
     stats.SetBorderSize(0)
-    stats.SetTextColor(2+2*0)
+    stats.SetTextColor(2+2*0)    ### red color 
     stats.SetX1NDC(0.15+0.55*0)
     stats.SetX2NDC(0.45+0.55*0)
-    stats.SetY1NDC(0.72)
-    stats.SetY2NDC(0.88)
+    if whichVtx == "vtx_x":
+        stats.SetY1NDC(0.12)
+        stats.SetY2NDC(0.28)
+    if whichVtx == "vtx_y":
+        stats.SetY1NDC(0.72)
+        stats.SetY2NDC(0.88)
 
     pad1.Modified()
     pad1.Update()
@@ -110,11 +121,16 @@ def makeCalibPlot(whichScan,rootOutFile,pdfOutFile):
     
     stats1 = vtxVsOff_B2.GetListOfFunctions().FindObject('stats')
     stats1.SetBorderSize(0)
-    stats1.SetTextColor(2+2*1)
+    stats1.SetTextColor(2+2*1)   ### blue color
     stats1.SetX1NDC(0.1+0.5*1)
     stats1.SetX2NDC(0.40+0.5*1)
-    stats1.SetY1NDC(0.72)
-    stats1.SetY2NDC(0.88)
+    if whichVtx == "vtx_x":
+        stats1.SetY1NDC(0.72)
+        stats1.SetY2NDC(0.88)
+    if whichVtx == "vtx_y":
+        stats1.SetY1NDC(0.12)
+        stats1.SetY2NDC(0.28)
+
     stats1.GetListOfLines().ls()
 
 
@@ -159,63 +175,3 @@ if __name__ == '__main__':
 
     makeCalibPlot("B1B2Y", "LScalib_B1B2Y_v1.root", "plotsLScalib_B1Y_"+string+"_p1.pdf")
     makeCalibPlot("B1B2X", "LScalib_B1B2X_v1.root", "plotsLScalib_B1X_"+string+"_p1.pdf")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
